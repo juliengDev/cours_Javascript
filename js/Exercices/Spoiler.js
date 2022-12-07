1591ffd9e64b67abaff7fd840178263325f6336d
Julien <juliengilbert.dev@gmail.com>
2022-12-07 05:18:11 +0100

    Ajout du projet Juste Prix

---
+++ js/Exercices/Spoiler.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector(".spoiler")
+let textBtn = document.querySelector(".spoiler + p")
+
+
+btn.addEventListener('click', () => {
+
+    if(btn.textContent === 'Afficher') {
+        btn.textContent = 'Cacher'
+        textBtn.textContent = 'Le texte est cache'
+    }else{
+        btn.textContent = 'Afficher'
+        textBtn.textContent = ''
+    }
+
+})
+
