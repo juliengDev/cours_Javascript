2158d530d45c69f31017c6b8cccb1a20cab292d9
Julien <juliengilbert.dev@gmail.com>
2022-12-09 19:41:53 +0100

    Ajout du cours cookies

--- js/Cours/Fetch_Axios/AXIOS/script.js
+++ js/Cours/Fetch_Axios/AXIOS/script.js
@@ -1,37 +1,47 @@
-const url = 'https://blockchain.info/ticker';
+// // ------------------------- Recuperer des donnees --------------------------
 
-async function recupererPrix() {
-  // Créer une requête
-//   let requete = new XMLHttpRequest(); // Créer un objet
-//   requete.open('GET', url); // Premier paramètre GET / POST, deuxième paramètr : url
-//   requete.responseType = 'json'; // Nous attendons du JSON
-//   requete.send(); // Nous envoyons notre requête
 
-//   // Dèss qu'on reçoit une réponse, cette fonction est executée
-//   requete.onload = function() {
-//     if (requete.readyState === XMLHttpRequest.DONE) {
-//       if (requete.status === 200) {
-//         let reponse = requete.response; // on stock la réponse
-//         let prixEnEuros = reponse.EUR.last;
-//         document.querySelector('span').textContent = prixEnEuros;
-//       }
-//       else {
-//         alert('Un problème est intervenu, merci de revenir plus tard.');
-//       }
-//     }
+// // const url = 'https://blockchain.info/ticker';
+
+// // function recupererPrix() {
+
+
+// //   axios.get(url)
+// //     .then(function(donnees) {
+// //       // console.log(donnees);
+// //       document.querySelector('span').textContent = donnees.data.EUR.last;
+// //     })
+// //     .catch(function(erreur) {
+// //       alert('Un probleme est survenu');
+// //     })
+// //     .then(function () {
+// //       console.log('mise a jour effectuee');
+// //     });
+// // }
+
+// // setInterval(recupererPrix, 1000) 
+
+
+// // ------------------------- Envoyer des donnees --------------------------
+
+// const url = 'https://lesoublisdelinfo.com/api.php';
+
+
+// const axiosInstancePost = axios.create({
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded'
 //   }
-//   console.log('Prix actualisé');
-  const requete = await fetch(url, {
-    method: 'GET'
-  });
-  
-  if(!requete.ok) {
-    alert('Un problème est survenu.');
-  } else {
-    let donnees = await requete.json();
-    // console.log(donnees);
-    document.querySelector('span').textContent = donnees.EUR.last;
-  }
-}
+// });
 
-setInterval(recupererPrix, 1000);
+// axiosInstancePost.post(url, new URLSearchParams({
+  
+//     prenom: 'Julien'  
+// }))
+
+// .then(function(donnees){
+//   console.log(donnees.data)
+// })
+
+// .catch(function(erreur){
+//   console.log(erreur)
+// })
