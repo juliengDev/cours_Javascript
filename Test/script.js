8d07f769682f81376164173f40349ccef9cd694c
Julien <juliengilbert.dev@gmail.com>
2022-12-21 11:53:46 +0100

    Ajout du cours sur la POO

--- Test/script.js
+++ Test/script.js
@@ -1,11 +1,25 @@
-let btn = document.querySelector('.btn');
-let p = document.querySelector('.text')
+// class MusicPlayer {
+//     constructor(filetype) {
+//         this.filetype = filetype;
+//     }
 
-btn.addEventListener('click', () => {
-    
-    let element = document.createElement('div');
-    element.textContent = 'Coucou fdp';
-    p.appendChild(element)    
-    
+// }
 
-})
+// MusicPlayer.prototype.filename = null;
+
+// MusicPlayer.prototype.hello = function() {
+//     console.log("Hello World !")
+// }
+
+// const winamp = new MusicPlayer('mp3\n')
+// console.log(winamp.filetype)
+// winamp.filename = 'Billy Jean'
+// console.log(winamp.filename)
+// winamp.hello();
+
+
+const chiffre1 = new Number(4)
+const chiffre2 = 4;
+
+console.log(chiffre1)
+console.log(chiffre2)
